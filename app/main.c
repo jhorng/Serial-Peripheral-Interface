c41c6c4f580ed90a027798471f6a78a216b82cc5
Chu Jaan Horng <jjaanhorng@gmail.com>
2015-12-09 16:11:51 +0800

    Error ensured.  Irregular sequence orders while initializing cause problems.  Problem solved by reordering the sequence.
     Method to solve the problem:
     1. Read back the value in the register in debugger mode.
     2. Compare the simulated value with the expected value.

--- app/main.c
+++ app/main.c
@@ -20,6 +20,7 @@ int main(){
 
 	/****
 	 *	Pin 2 = SPI_SCK
+	 *	Pin 4 = NSS
 	 *	Pin 5 = SPI_MISO
 	 *	Pin 6 = SPI_MOSI
 	 ****/
@@ -28,6 +29,11 @@ int main(){
 	//pullUpDown(PIN_2, PORTE, GPIO_PULL_DOWN);
 	altFunction(PIN_2, PORTE, AF5);
 	//outputType(PIN_2, PORTE, GPIO_PUSH_PULL);
+
+	configurePin(GPIO_MODE_ALTFUNC, PIN_4, PORTE);
+	//pullUpDown(PIN_4, PORTE, GPIO_PULL_DOWN);
+	altFunction(PIN_4, PORTE, AF5);
+	//outputType(PIN_4, PORTE, GPIO_PUSH_PULL);
 
 	configurePin(GPIO_MODE_ALTFUNC, PIN_5, PORTE);
 	//pullUpDown(PIN_5, PORTE, GPIO_PULL_DOWN);
@@ -38,15 +44,6 @@ int main(){
 	//pullUpDown(PIN_6, PORTE, GPIO_PULL_UP);
 	altFunction(PIN_6, PORTE, AF5);
 	//outputType(PIN_6, PORTE, GPIO_PUSH_PULL);
-
-	/****
-	 * Pin 4 = SPI_NSS
-	 ****/
-
-	configurePin(GPIO_MODE_ALTFUNC, PIN_4, PORTE);
-	//pullUpDown(PIN_4, PORTE, GPIO_PULL_DOWN);
-	altFunction(PIN_4, PORTE, AF5);
-	//outputType(PIN_4, PORTE, GPIO_PUSH_PULL);
 
 	//configureSPI(UniDirec_2_Line, Transmit, Receive_Only_Disable, TI_Mode, Master_Mode, Bit16, MSB, Clock1, CRCdisable, SSMenable, BR1, SSOE_Enable);
 	//configureSPI(BiDirec_1_Line, Receive, Receive_Only_Disable, TI_Mode, Bit8, MSB, Clock1, CRCdisable, SSMenable, BR1, SSOE_Enable);
@@ -60,7 +57,7 @@ int main(){
 	//configureReceive(Receive_Only_Enable);
 	//configureDataFrame(Bit8);
 	//enableCRC(CRCdisable);
-	configureBR(BR4);
+	configureBR(BR1);
 	//enableSSM(SSMdisable);
 	//configureLSBFIRST(MSB);
 	//configureClock(Clock1);
@@ -76,13 +73,12 @@ int main(){
 	//writeData = 0x1;
 
 	while(1){
-		configureOutput(Transmit);
 		//writeOne(PIN_4, PORTE);
 		//delay(10000);
 		//writeZero(PIN_4, PORTE);
 		//delay(10);
 		//delay(1000);
-		sendData(0x08);
+		sendData(0x6D);
 		//readData = SPI_reg->SPI_DR;
 		//delay(1000);
 		//status = SPI_reg->SPI_SR;
